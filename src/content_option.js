c8c186b8735271f94ce5ce763d66750913d9472d
Ibrahim Salah <57736764+IbrahimSalahEldin@users.noreply.github.com>
2025-07-21 22:06:52 +0400

    adds

---
+++ src/content_option.js
@@ -0,0 +1,224 @@
+import tawafonaImg from "./assets/images/projects/tawafona.png";
+import nmaImage from "./assets/images/projects/nmaColl.png";
+import hmazaImage from "./assets/images/projects/hamza (2).png";
+import qnvexImage from "./assets/images/projects/qnvex.png";
+import tripImage from "./assets/images/projects/tripVex.png";
+import mainImg from "./assets/images/main.jpeg";
+
+const logotext = "ISABELLE";
+const meta = {
+  title: "ISABELLE AIRLINES",
+  description:
+    "A modern and reliable airline offering safe and comfortable travel experiences.",
+};
+
+const introdata = {
+  title: "I’m Isabelle",
+  animated: {
+    first: "I’ve served in strategic aviation and diplomacy for over 17 years.",
+    second:
+      "I’ve flown world leaders, managed high-security missions, and trained as a licensed flight instructor.",
+    third:
+      "Now, I lead Isabelle Airlines, offering elite private aviation consultancy worldwide.",
+  },
+  description:
+    "Founder of Isabelle Airlines, specializing in executive private aviation consulting for VIP and government clients. Background in aviation strategy, high-level protocol, and international operations. Passionate about precision, confidentiality, and delivering elite travel experiences.",
+  your_img_url: mainImg,
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Isabelle began her career at the age of 19 with the Ministry of National Defence, where she entered the field of strategic operations and official delegation support. Over the course of 1 years, she served alongside 30 Heads of State, participating in high-level international engagements across diplomatic, defense, and executive contexts. As a licensed flight instructor with over 10,000 flight hours, she demonstrated a deep command of aviation discipline, precision and mission-aligned execution. Over a span of 17 years, Isabelle served under Romanian Presidents Ion Iliescu,Emil Constantinescu, Traian Basescu, Nicolae Vacaroiu involving other 26 Heads of State. As well, she was honoured with a Medal ofHonour – flying troops to war zones and civil war – IRAQ and ANGOLA. Her involvement required strict compliance with diplomatic protocol, movement security and classified coordination standards functioning within frameworks of both political sensitivity and logistical complexity. Following her national service, Isabelle advanced as a political strategist responsible for developing a comprehensive and dynamic plan that aligns with the candidate's vision, members of the Senate and Chamber of Deputies of Romania. Campaign Strategy & Planning Political Positioning & Messaging Advisory to Political Leadership Stakeholder Coordination Supervise security, confidentiality, and reputational containment across events. She is the founder of Isabelle Airlines, a private aviation consultancy serving high-net-worth individuals, political figures.",
+};
+const worktimeline = [
+  {
+    jobtitle: "FrontEnd Developer",
+    where: "QuanVex Company in Egypt",
+    date: "2024",
+  },
+  {
+    jobtitle: "Software Engineer And Web Developer",
+    where: "FreeLancer",
+    date: "2022",
+  },
+];
+
+const skills = [
+  {
+    name: "Html",
+    value: 99,
+  },
+  {
+    name: "Css3",
+    value: 95,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Bootstrap",
+    value: 90,
+  },
+  {
+    name: "Tailwindcss",
+    value: 90,
+  },
+  {
+    name: "Shadcn",
+    value: 70,
+  },
+  {
+    name: "Reactjs",
+    value: 85,
+  },
+  {
+    name: "Nextjs",
+    value: 60,
+  },
+  {
+    name: "WordPress",
+    value: 60,
+  },
+  {
+    name: "Nodejs",
+    value: 35,
+  },
+  {
+    name: "Mongoose",
+    value: 35,
+  },
+  {
+    name: "Express",
+    value: 35,
+  },
+  {
+    name: "PHP",
+    value: 35,
+  },
+];
+
+const services = [
+  {
+    title: "FrontEnd Developer",
+    description:
+      "Crafting responsive, user-friendly interfaces using modern frameworks like React.js and Next.js.    Focused on clean code, performance, and seamless user experiences.",
+  },
+  {
+    title: "Web Apps",
+    description:
+      "Building scalable, high-performance web applications using modern frameworks like React.js and Next.js — tailored for seamless user experiences and real-world functionality.",
+  },
+  {
+    title: "WordPress Design",
+    description:
+      "Custom, responsive WordPress websites tailored to your brand. From sleek business pages to dynamic blogs, I create user-friendly designs optimized for performance and easy management.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: tawafonaImg,
+    description:
+      "Tawafona is a smart platform that helps pilgrims easily book and manage Hajj and Umrah trips.",
+    link: "https://tawafona.com/en",
+  },
+  {
+    img: nmaImage,
+    description:
+      "nmacool is an online platform offering a wide range of Apple products, tech gadgets, and accessories, along with repair and support services.",
+    link: "https://nmacool.com/en",
+  },
+  {
+    img: qnvexImage,
+    description:
+      "QuanVex is a technology company specializing in innovative software solutions, offering services in web development, cloud solutions, and digital transformation for businesses.",
+    link: "https://www.quanvex.com/",
+  },
+  {
+    img: hmazaImage,
+    description:
+      "Hamza Quran Academy offers online Quran education with personalized lessons, helping students of all ages learn and memorize the Quran in a flexible, interactive, and accessible manner.",
+    link: "https://hamzahquranacademy.com/",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "Eljaeza freelancer project (Laravel- mysql - reactjs)",
+    link: "#",
+  },
+  {
+    img: tripImage,
+    description:
+      "Tripvex is a tourism platform that helps travelers discover, plan, and book unique experiences, offering curated tours, accommodations, and activities for a personalized travel adventure.",
+    link: "#",
+  },
+
+  // {
+  //   img: "https://picsum.photos/400/600/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/300/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/550/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/700/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "isabelleairline@gmail.com",
+  YOUR_FONE: "(+971)58 196 9927",
+  description:
+    "Get in touch for inquiries, collaborations, or to learn more about Isabelle Airlines services.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_d7lvv1w",
+  YOUR_TEMPLATE_ID: "template_1zop4me",
+  YOUR_USER_ID: "DP0GCTSj6RhdbThUh",
+};
+
+const socialprofils = {
+  github: "https://github.com/IbrahimSalahEldin",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/ibrahim-salah-29850517b/",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
